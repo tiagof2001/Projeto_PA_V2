edb22eed68d04f2082bf5b35457a6ad8ef9deae8
vsantos03 <santos.vasco2003@gmail.com>
2025-05-18 15:28:13 +0100

    Update do JsonString e do JsonValue.kt com documentação

--- src/jsonAlternative/JsonString.kt
+++ src/jsonAlternative/JsonString.kt
@@ -8,7 +8,22 @@ import visitorToJson.JsonVisitor
  */
 class JsonString(private var value: String) : JsonValue{
 
+    /**
+     * Converte a instância actual para a sua representação em string JSON.
+     *
+     * @return Uma string JSON que representa a instância atual, com o valor da string
+     *         devidamente codificado e entre aspas duplas.
+     */
+
     override fun toJson(): String = "\"${value}\""
+
+    /**
+     * Aceita um `JsonVisitor` para processar ou percorrer a instância actual de `JsonString`.
+     *
+     * Este método invoca o método `visitorString` do visitor fornecido, passando a instância atual.
+     *
+     * @param visitor Uma implementação de `JsonVisitor` que define o comportamento para tratar instâncias de `JsonString`.
+     */
 
     override fun accept(visitor: JsonVisitor) {
         visitor.visitorString(this)
